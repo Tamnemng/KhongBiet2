3bd229b64605e92a6d4dad5a3244f46bb923266e
Tamnemng <nguyndantdm6@gmail.com>
2025-04-21 20:21:12 +0700

    first commit

---
+++ SportStore/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+
+    }
+}
---
+++ SportStore/Pages/Cart.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
+using SportStore.Models;
+
+namespace SportStore.Pages
+{
+    public class CartModel : PageModel
+    {
+        private readonly IStoreRepository repository;
+        public Cart Cart { get; set; }
+        public string ReTurnUrl { get; set; } = "/";
+        public CartModel(IStoreRepository repo, Cart cartService)
+        {
+            repository = repo;
+            Cart = cartService;
+        }
+        public void OnGet(string reTurnUrl)
+        {
+            ReTurnUrl = reTurnUrl ?? "/";
+        }
+        public IActionResult OnPost(long productId, string reTurnUrl)
+        {
+            Product? product = repository.GetProducts.FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart.AddItem(product, 1);
+            }
+            return RedirectToPage(new { reTurnUrl = reTurnUrl });
+        }
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == productId).Product);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+    }
+}
